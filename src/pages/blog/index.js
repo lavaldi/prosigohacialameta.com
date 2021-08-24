3569e48eed28cac3c139cfb5d30e8f5369474f72
Claudia Valdivieso <claumavaldivieso@gmail.com>
2021-08-24 18:17:27 -0500

    add search in blog page with Fuse
    close #8

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,6 +1,9 @@
 /** @jsx jsx */
-import { jsx } from 'theme-ui'
-import { graphql } from 'gatsby'
+import { jsx, Input } from 'theme-ui'
+import React, { useState } from 'react'
+import { graphql, navigate } from 'gatsby'
+import Fuse from 'fuse.js'
+import * as queryString from 'query-string'
 import { ArticleList } from '../../components/article-list'
 
 export const query = graphql`
@@ -23,6 +26,8 @@ export const query = graphql`
         slug
         frontmatter {
           title
+          tags
+          excerpt
           date
           draft
         }
@@ -32,9 +37,67 @@ export const query = graphql`
 `
 
 const Blog = props => {
-  const posts = props.data.pages.nodes
+  const { location, data } = props;
 
-  return <ArticleList posts={posts} />
+  const posts = data.pages.nodes.map(n => ({...n, ...n.frontmatter}))
+
+  const { search: searchParam = '' } = queryString.parse(location.search)
+  
+  const fuseOptions = {
+    keys: [
+      { name: 'title', weight: 0.8 },
+      { name: 'excerpt', weight: 0.5 },
+      { name: 'tags', weight: 0.2 },
+    ],
+    useExtendedSearch: true,
+  }
+  const fuse = new Fuse(posts, fuseOptions)
+
+  const [query, setQuery] = useState(searchParam);
+  
+  const result = fuse.search(searchParam)
+  const resultPosts = result.map(res => res.item)
+
+  return (
+    <>
+      <h1>Artículos</h1>
+      <div className="relative rounded-md shadow-sm max-w-lg mx-auto" sx={{
+        position: 'relative',
+        borderRadius: '0.375em',
+        boxShadow: '0 1px 2px 0 rgb(0 0 0 / 5%)',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        maxWidth: '32em'
+      }}>
+        <label htmlFor="search" sx={{
+          position: 'absolute',
+          width: '1px',
+          height: '1px',
+          padding: 0,
+          margin: '-1px',
+          overflow: 'hidden',
+          whiteSpace: 'nowrap',
+          borderWidth: 0,
+        }}>
+          Search
+        </label>
+        <Input
+          autoComplete="off"
+          aria-labelledby="search"
+          type="search"
+          id="search"
+          value={query}
+          onChange={(event) => {
+            event.preventDefault()
+            navigate(event.target.value ? `/blog/?search=${event.target.value}` : '/blog')
+            setQuery(event.target.value)
+          }}
+          placeholder="Search posts"
+        />
+      </div>
+      <ArticleList posts={searchParam.length > 0 ? resultPosts : posts} />
+    </>
+  )
 }
 
 export default Blog;
