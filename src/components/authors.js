93081c377a30e297b259df82600f4dabade5356b
Claudia Valdivieso <claumavaldivieso@gmail.com>
2021-09-03 21:16:24 -0500

    fix eslint warning

--- src/components/authors.js
+++ src/components/authors.js
@@ -1,7 +1,9 @@
-export default {
+const authors = {
   "paul-tripp": {
     avatar: "/authors/paultripp.jpg",
     name: "Paul Tripp",
     description: "El Dr. Paul David Tripp es pastor, conferencista internacional y autor de libros éxito de ventas y ganadores de premios. Es el director de Paul Tripp Ministries. Con más de 30 libros y series en video, la pasión que mueve a Paul es conectar el poder transformador de Jesucristo a la vida cotidiana."
   }
 }
+
+export default authors
