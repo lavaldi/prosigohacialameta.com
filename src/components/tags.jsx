0ad0cbcf460fdf9174e33c7dc820b1dd67365553
Claudia Valdivieso <claumavaldivieso@gmail.com>
2021-10-24 10:43:30 -0500

    tags and tag pages
    close #4

---
+++ src/components/tags.jsx
@@ -0,0 +1,36 @@
+/** @jsx jsx */
+import { jsx, Link as TLink, Heading, Box, Flex } from "theme-ui"
+import kebabCase from "lodash.kebabcase"
+import { Link } from "gatsby"
+import Layout from "./layout"
+import useMinimalBlogConfig from "../hooks/use-minimal-blog-config"
+import Seo from "./seo"
+import replaceSlashes from "../utils/replace-slashes"
+
+const Tags = ({ list }) => {
+  const { tagsPath, basePath } = useMinimalBlogConfig()
+
+  return (
+    <Layout>
+      <Seo title="Tags" />
+      <Heading as="h1" variant="styles.h1">
+        Tags
+      </Heading>
+      <Box mt={[4, 5]}>
+        {list.map((listItem) => (
+          <Flex key={listItem.fieldValue} mb={[1, 1, 2]} sx={{ alignItems: `center` }}>
+            <TLink
+              as={Link}
+              sx={{ variant: `links.listItem`, mr: 2 }}
+              to={replaceSlashes(`/${basePath}/${tagsPath}/${kebabCase(listItem.fieldValue)}`)}
+            >
+              {listItem.fieldValue} <span sx={{ color: `secondary` }}>({listItem.totalCount})</span>
+            </TLink>
+          </Flex>
+        ))}
+      </Box>
+    </Layout>
+  )
+}
+
+export default Tags
