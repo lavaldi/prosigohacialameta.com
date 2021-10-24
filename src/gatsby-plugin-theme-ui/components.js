39feeb34475b18d0cf876185166a5cac94ff9e8f
Claudia Valdivieso <claumavaldivieso@gmail.com>
2021-10-23 21:10:02 -0500

    set theme-ui
    close #16

---
+++ src/gatsby-plugin-theme-ui/components.js
@@ -0,0 +1,16 @@
+/* eslint react/prop-types: 0 */
+import * as React from "react";
+import { Text } from "theme-ui";
+import Title from "../components/title";
+
+const components = {
+  Text: ({ children, ...props }) => <Text {...props}>{children}</Text>,
+  Title: ({ children, text, ...props }) => (
+    <Title text={text} {...props}>
+      {children}
+    </Title>
+  ),
+  wrapper: ({ children }) => <>{children}</>,
+};
+
+export default components;
